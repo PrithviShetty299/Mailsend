01192a812f7bbdb8ec5a7e6c2f48ae24cff40199
Prithvi Shetty <prithvinshetty.aadi@gmail.com>
2020-08-19 14:25:51 +0530

    Add files via upload

---
+++ send_script.php
@@ -0,0 +1,29 @@
+<?php 
+if (isset($_POST['send_message_btn'])) {
+  $name = $_POST['name'];
+  $email = $_POST['email'];
+  $subject = $_POST['subject'];
+  $msg = $_POST['msg'];
+  // Content-Type helps email client to parse file as HTML 
+  // therefore retaining styles
+  //$headers = "MIME-Version: 1.0" . "\r\n";
+  $headers = "From: prithvinshetty.aadi@gmail.com" . "\r\n";
+  $headers .= "Content-type:text/html;charset=UTF-8" . "\r\n";
+  $message = "<html>
+  <head>
+  	<title>New message from website contact form</title>
+  </head>
+  <body>
+  	<h1>" . $subject . "</h1>
+  	<p>".$msg."</p>
+  </body>
+  </html>";
+  //ini_set('SMTP','localhost');
+  //ini_set('smtp_port','25');
+  if (mail($email, $subject, $message, $headers)) {
+   echo "Email sent";
+  }else{
+   echo "Failed to send email. Please try again later";
+  }
+}
+?>
